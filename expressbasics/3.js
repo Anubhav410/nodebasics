9a6dd176982472ec6725fac025a5391ced07d518
anubhav <anubhaviam@gmail.com>
2017-08-10 15:50:29 +0530

    adding basic express js codes

---
+++ expressbasics/3.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var pug = require('pug');
+
+app.set('views' , process.argv[3]);
+app.set('view engine' , 'pug');
+
+app.get('/home' ,  function( req,res ){
+
+	res.render( 'index' , { date : new Date().toDateString() } );
+
+});
+
+app.listen( process.argv[2] );
