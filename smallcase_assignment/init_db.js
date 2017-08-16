5f7fab48d0ca93c27fcafb29181c18eaadd8aa4f
anubhav <anubhaviam@gmail.com>
2017-08-16 11:41:12 +0530

    done with holdings and init db script

---
+++ smallcase_assignment/init_db.js
@@ -0,0 +1,84 @@
+var mongo = require('mongodb');
+var mongoClientUrl = "mongodb://localhost:27017/smallcasedb";
+
+mongo.MongoClient.connect(  mongoClientUrl , function(err , db){
+		
+	db.collection('stocks').drop();
+	db.collection('trades').drop();
+
+	console.log('Adding stocks');
+
+	db.collection('stocks').insert( [ { name : 'reliance' } , { name : 'hdfcbank' } ] , function( err , data ){
+		if( err != null ){
+			console.log( err );
+			db.close();
+			return;
+		}
+
+		console.log( data );
+	});
+
+
+	console.log( 'Adding trades for reliance' );
+	db.collection('trades').insert( [ { 
+										stockName : 'reliance' ,
+										type 	  : 'buy',
+										quantity  : '100',
+										price     : '900',
+										date      : '2015-04-10'
+									   },
+									   { 
+										stockName : 'reliance' ,
+										type 	  : 'sell',
+										quantity  : '50',
+										price     : '1000',
+										date      : '2015-05-10'
+									   },
+									   { 
+										stockName : 'reliance' ,
+										type 	  : 'buy',
+										quantity  : '100',
+										price     : '850',
+										date      : '2015-06-10'
+									   } ] , function(err , data){
+
+									   			if( err != null ){
+													console.log( err );
+													db.close();
+													return;
+												}
+												console.log( data );
+
+									   } );
+
+	console.log( 'Adding trades for hdfcbank' );
+	db.collection('trades').insert( [ { 
+										stockName : 'hdfcbank' ,
+										type 	  : 'buy',
+										quantity  : '200',
+										price     : '100',
+										date      : '2015-05-11'
+									   },
+									   { 
+										stockName : 'hdfcbank' ,
+										type 	  : 'sell',
+										quantity  : '100',
+										price     : '800',
+										date      : '2015-07-12'
+									   }
+									   ] , function(err , data){
+
+									   			if( err != null ){
+													console.log( err );
+													db.close();
+													return;
+												}
+												console.log( data );
+
+									   } );
+
+
+	db.close();
+	console.log('Init Db Complete');
+
+});
--- smallcase_assignment/main.js
+++ smallcase_assignment/main.js
@@ -8,24 +8,87 @@ var app = express();
 app.use( bodyparser.urlencoded( {extended : false} ) );
 
 app.get('/portfolio/' , connectDB ,function( req, res ){
-
 	var db = req.db;
-	//read all the one portfolio in the collection and throw it back
-	var stocks = db.collection('stocks').find();
+	//get all the trades, as it has all the basic info we need
+	db.collection("trades").find().toArray().then( function( tradeInfo ){
+		var result = sortTrades( tradeInfo );
+		res.end( JSON.stringify(result) );
+	} ).catch(err => {
+	    console.error(err);
+	    return err; 
+  	});
 
-	var result = "";
+	/*	stocks.toArray().then( function( data ){
+			//here you have both the stocks, now you get the list of trades for these guys
+			var all_promises = [];
+			for( var i = 0 ; i < data.length ; i++ ){
+				all_promises.push(
+					db.collection("trades").find( { stockId : data[i]._id.toString() } ).toArray().then( function( trade ){
+						return {
+							stockName : "aniuasiunb",
+							trades : JSON.stringify(trade)
 
-	stocks.forEach( function( e , i ){
+						}
+				} 
+				));
+			}
+			Promise.all( all_promises ).then( function( data ){
+				var all_trades = data;
+				console.log(data);
+			} );
 
-		var temp = { "name" : e.name };
-		var trade_info = [];
-		var trades = db.collection('trades')
-					   .find( { "stock_id" : "598efdefcfac228d368625ec" } )
-					   .forEach( function( data ){
-							trade_info.push( { "count" : data.count } );
-						});
-	} );
-	
+		} ).then(function(){
+			console.log("final done");
+		});
+	*/
+});
+
+app.get('/portfolio/holdings' , connectDB ,function( req, res ){
+	var db = req.db;
+	//get all the trades, as it has all the basic info we need
+	db.collection("trades").find().toArray().then( function( tradeInfo ){
+		var allTrades = sortTrades( tradeInfo );
+		var result = {};
+		for( let key in allTrades ){
+			var trades = allTrades[key];
+			//now loop through the trades, to calculate the average buying price
+
+			var totalShares = 0;
+			var finalTotalPrice = 0;
+			var totalBuyShares = 0;
+
+			for( var  i = 0; i < trades.length ; i++ ){
+				var price = parseInt( trades[i]['price'] );
+				var quantity = parseInt( trades[i]['quantity'] );
+				switch( trades[i]['type'] ){
+
+					case 'buy' : 
+						totalShares += quantity;
+						totalBuyShares += quantity;
+						finalTotalPrice += quantity * price;
+					break;
+
+					case 'sell' : 
+						totalShares -= quantity;
+					break;
+				}
+			}
+			result[ key ] = {
+				quantity : totalShares , 
+				price    : finalTotalPrice/totalBuyShares
+			}
+		}
+
+		res.end( JSON.stringify( result ) );
+
+	} ).catch(err => {
+	    console.error(err);
+	    return err; 
+  	});
+});
+
+app.get('/portfolio/returns' , connectDB ,function( req, res ){
+
 
 });
 
@@ -33,10 +96,15 @@ app.get('/portfolio/' , connectDB ,function( req, res ){
 app.post( "/portfolio/addTrade" , connectDB  ,function( req , res ){
 
 	/* to add a trade, you send the stock_id, the quantity , price , date and type(buy/sell) */
-	// console.log( req.body.str );
+
+	console.log('Adding trade');
+
+	if( req.body.stockId ==  undefined || req.body.quantity == undefined || req.body.price ==  undefined || req.body.type == undefined || req.body.date == undefined  ){
+		res.end('Bad params');
+	}
 
 	var trade = { 
-					"stockId" : req.body.stockId,
+					"stockName" : req.body.stockName,
 					"quantity" : req.body.quantity,
 					"price" : req.body.price,
 					"type" : req.body.type,
@@ -45,8 +113,10 @@ app.post( "/portfolio/addTrade" , connectDB  ,function( req , res ){
 	req.db.collection('trades').insert( trade , function( err , data ){
 		console.log( JSON.stringify(err) );
 		if( err != null ){
+			console.log("Error : " + err);
 			res.end( JSON.stringify(data.err) );
 		}else{
+			console.log( "Trade Deleted : "  + JSON.stringify(data));
 			res.end( JSON.stringify(data.result) );
 		}
 	} );	
@@ -71,13 +141,17 @@ app.post( "/portfolio/updateTrade" , connectDB  ,function( req , res ){
 		newVal['type'] = req.body.type;
 	}
 
-	console.log( req.body );
+	if( req.body.tradeId == undefined || newVal.length == 0 ){
+		res.end( 'Bad params' );
+	}
 
 	req.db.collection('trades').update( { _id : mongo.ObjectID(req.body.tradeId) } , { $set :  newVal } , function( err , data ){
 		
 		if( err != null ){
+			console.log("Error : " + err);
 			res.end( JSON.stringify(err) );
 		}else{
+			console.log( "Trade Deleted : "  + JSON.stringify(data));
 			res.end( JSON.stringify(data) );
 		}
 	} );	
@@ -86,11 +160,19 @@ app.post( "/portfolio/updateTrade" , connectDB  ,function( req , res ){
 } );
 
 app.post( "/portfolio/removeTrade" , connectDB  ,function( req , res ){
-	req.db.collection('trades').remove( { _id : mongo.ObjectID(req.body.tradeId) } , { $set :  newVal } , function( err , data ){
+	console.log( "Deleteing trade : " +  req.body.tradeId);
+
+	if( req.body.tradeId == undefined ){
+		res.end( 'Bad params' );
+	}
+
+	req.db.collection('trades').remove( { _id : mongo.ObjectID(req.body.tradeId) } , function( err , data ){
 		
 		if( err != null ){
+			console.log("Error : " + err);
 			res.end( JSON.stringify(err) );
 		}else{
+			console.log( "Trade Deleted : "  + JSON.stringify(data));
 			res.end( JSON.stringify(data) );
 		}
 	} );	
@@ -101,6 +183,25 @@ app.post( "/portfolio/removeTrade" , connectDB  ,function( req , res ){
 
 app.listen( 3000 );
 
+
+function sortTrades( allTrades ){
+	var result = {};
+	for( var i = 0 ; i < allTrades.length ; i++ ){
+		data = allTrades[i];
+		if( result[ data['stockName'] ] == undefined ){
+			result[data['stockName']] = [];
+		}
+
+		var trade = {
+			quantity : data['quantity'],
+			price : data['price'],
+			type : data['type'],
+			date : data['date']
+		}
+		result[data['stockName']].push( trade );
+	}
+	return result;
+}	
 
 
 /*
